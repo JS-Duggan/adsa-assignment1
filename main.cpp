2b35bc429b43d84767ae25a79a3812dc08decb5b
Jack Duggan <jacksdggn@gmail.com>
2025-07-28 23:28:44 +0930

    feat: fix for large inputs by using string

--- main.cpp
+++ main.cpp
@@ -2,29 +2,27 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <string>
 
-std::vector<int> int2vec(int i) {
+std::vector<int> string2vec(std::string i) {
   std::vector<int> vec;
-  while (i > 0) {
-    vec.push_back(i % 10);
-    i /= 10;
+  for (auto it = i.rbegin(); it != i.rend(); it++) {
+    vec.push_back(*it - '0');
   }
   return vec;
 }
 
-int vec2int(std::vector<int> vec) {
-  int i = 0;
-  for (size_t j = 0; j < vec.size(); j++) {
-    i += vec[j] * pow(10, j);
+void outputVec(std::vector<int> vec) {
+  for (auto it = vec.rbegin(); it != vec.rend(); it++) {
+    std::cout << *it;
   }
-  return i;
 }
 
 int safeGet(std::vector<int> vec, size_t i) {
   return (i < vec.size()) ? vec[i] : 0;
 }
 
-int schoolAddition(std::vector<int> i1, std::vector<int> i2, int b) {
+std::vector<int> schoolAddition(std::vector<int> i1, std::vector<int> i2, int b) {
   size_t length = std::max(i1.size(), i2.size());
   int carry = 0;
   std::vector<int> ans(length + 1, 0);
@@ -40,7 +38,8 @@ int schoolAddition(std::vector<int> i1, std::vector<int> i2, int b) {
     }
     ans[i] = sum;
   }
-  return vec2int(ans);
+  if(!ans.empty() && ans.back() == 0) ans.pop_back();
+  return ans;
 }
 
 int karatsubaMultiplication(std::vector<int> i1, std::vector<int> i2, int b) {
@@ -54,13 +53,16 @@ int ratio(std::vector<int> i1, std::vector<int> i2, int b) {
 }
 
 int main() {
-  int i1, i2, b;
+  std::string i1, i2;
+  int b;
   std::cin >> i1 >> i2 >> b;
-  std::vector<int> vec1 = int2vec(i1);
-  std::vector<int> vec2 = int2vec(i2);
-  int sum = schoolAddition(vec1, vec2, b);
-  int product = karatsubaMultiplication(vec1, vec2, b);
-  int quotient = ratio(vec1, vec2, b);
-  std::cout << sum << " " << product << " " << quotient << std::endl;
+  std::cout << i1 << " " << i2 <<  " " << b << std::endl;
+  std::vector<int> vec1 = string2vec(i1);
+  std::vector<int> vec2 = string2vec(i2);
+  std::vector<int> sum = schoolAddition(vec1, vec2, b);
+  // int product = karatsubaMultiplication(vec1, vec2, b);
+  // int quotient = ratio(vec1, vec2, b);
+  outputVec(sum);
+  std::cout << " " << 0 << " " << 0 << std::endl;
   return 0;
 }
