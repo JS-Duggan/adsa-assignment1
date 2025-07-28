8dd9137abe8152652c863b6997f9f991843435df
Jack Duggan <jacksdggn@gmail.com>
2025-07-28 16:49:21 +0930

    feat: add vec2int func

--- main.cpp
+++ main.cpp
@@ -2,18 +2,6 @@
 #include <vector>
 #include <algorithm>
 
-int main() {
-  int i1, i2, b;
-  std::cin >> i1 >> i2 >> b;
-  std::vector<int> vec1 = int2vec(i1);
-  std::vector<int> vec2 = int2vec(i2);
-  int sum = schoolAddition(vec1, vec2, b);
-  int product = karatsubaMultiplication(vec1, vec2, b);
-  int quotient = ratio(vec1, vec2, b);
-  std::cout << sum << " " << product << " " << quotient << std::endl;
-  return 0;
-}
-
 int schoolAddition(std::vector<int> i1, std::vector<int> i2, int b) {
   int sum;
   return sum;
@@ -35,6 +23,26 @@ std::vector<int> int2vec(int i) {
     vec.push_back(i % 10);
     i /= 10;
   }
-  std::reverse(vec.begin(), vec.end());
   return vec;
+}
+
+int vec2int(std::vector<int> vec) {
+  int i = 0;
+  for (size_t j = 0; j < vec.size(); j++) {
+    std::cout << vec[j] << " ";
+    i += vec[j] * pow(10, j);
+  }
+  return i;
+}
+
+int main() {
+  int i1, i2, b;
+  std::cin >> i1 >> i2 >> b;
+  std::vector<int> vec1 = int2vec(i1);
+  std::vector<int> vec2 = int2vec(i2);
+  int sum = schoolAddition(vec1, vec2, b);
+  int product = karatsubaMultiplication(vec1, vec2, b);
+  int quotient = ratio(vec1, vec2, b);
+  std::cout << sum << " " << product << " " << quotient << std::endl;
+  return 0;
 }
